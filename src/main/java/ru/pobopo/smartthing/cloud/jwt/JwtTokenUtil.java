42751f708086bb0b5a8db5b2fb54af464f76bd93
PavelProjects <paveloalexeev52@gmail.com>
2023-10-24 19:18:22 +0300

    compose update

--- src/main/java/ru/pobopo/smartthing/cloud/jwt/JwtTokenUtil.java
+++ src/main/java/ru/pobopo/smartthing/cloud/jwt/JwtTokenUtil.java
@@ -60,17 +60,13 @@ public class JwtTokenUtil {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
-    //while creating the token -
-    //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-    //2. Sign the JWT using the HS512 algorithm and secret key.
-    //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
-    //   compaction of the JWT to a URL-safe string
-    public String doGenerateToken(String subject, Map<String, Object> claims, long lifeTime) {
+    // ttl - seconds
+    public String doGenerateToken(String subject, Map<String, Object> claims, long ttl) {
         return Jwts.builder()
             .setClaims(claims)
             .setSubject(subject)
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + lifeTime))
+            .setExpiration(new Date(System.currentTimeMillis() + ttl * 1000))
             .signWith(key, SignatureAlgorithm.HS512)
             .compact();
     }
